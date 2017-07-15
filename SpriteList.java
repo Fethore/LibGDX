066011834f1f61a082a8eaf2023f908b78194a57
Fethore <angel_ferndz@live.com>
2017-07-14 21:20:23 -0400

    Create SpriteList.java
    Created to help organize sprites.

---
+++ SpriteList.java
@@ -0,0 +1,27 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import java.util.ArrayList;
+
+/** Created by Mike Fernandez on 7/14/2017 */
+
+public class SpriteList {
+    ArrayList<Sprite> sprites;
+
+    public SpriteList(){
+        sprites = new ArrayList<Sprite>();
+    }
+
+    public void addSprite(String SpriteLocation){
+        sprites.add(new Sprite(new Texture(Gdx.files.internal(SpriteLocation))));
+    }
+
+    public int getSize(){
+        return sprites.size();
+    }
+
+    public Sprite getSprite(int SpriteIndex){
+        return sprites.get(SpriteIndex);
+    }
+
+}
