47c5f6801a23d9bbc492f29c991ac136a63f33d7
Angel Fernandez <mike.ferndz@outlook.com>
2017-12-05 23:54:16 -0500

    Refactored

--- AnimationList.java
+++ AnimationList.java
@@ -3,38 +3,67 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import java.util.ArrayList;
 
-/** Created by Angel Fernandez on 7/14/2017 */
+/**
+ * Created by Angel Fernandez on 11/24/2017.
+ */
+public class Animations {
 
-public class AnimationList {
-   static ArrayList<Animation> animations;
+    private static ArrayList<Animation> animations;
+    private static final int fps = 7;
 
-    public static void load(){
+    public static void load() {
         animations = new ArrayList<Animation>();
-        add("falling-Sheet.png", 2, 2);
     }
 
-    public static void add(String SpriteSheet, int Columns, int Rows, float Speed){
-
-        Texture spriteSheet = new Texture(SpriteSheet);
-        int width = spriteSheet.getWidth();
-        int height = spriteSheet.getHeight();
-
-        TextureRegion[][] tmp = TextureRegion.split(spriteSheet, width / Columns, height / Rows);
-
-        TextureRegion[] animationFrames = new TextureRegion[Columns * Rows];
-        int index = 0;
-
-        for (int i = 0; i < Rows; i++){
-            for(int j = 0; j < Columns; j++) {
-                animationFrames[index++] = tmp[j][i];
-            }
-        }
-
-        animations.add(new Animation(Speed, animationFrames));
+    //============================================================================================= PUBLIC
+    private static void add(String SpriteSheet) {
+        animations.add(create(SpriteSheet, false));
     }
 
-    public static Animation get(int AnimationIndex){
+    public static Animation get(int AnimationIndex) {
         return animations.get(AnimationIndex);
+    }
+
+    public static Animation create(String SpriteSheet) {
+        return create(SpriteSheet, false);
+    }
+
+    public static Animation create(String SpriteSheet, boolean Flip) {
+        Texture spriteSheet = new Texture(SpriteSheet);
+        TextureRegion[][] tmp = createTextureRegion(spriteSheet);
+        int length = countFrames(spriteSheet);
+        if (Flip) { tmp = flipTextures(tmp, length);}
+        return new Animation(Speed(length), createAnimationFrames(tmp, length));
+    }
+
+    //============================================================================================= PRIVATE
+    private static TextureRegion[] createAnimationFrames(TextureRegion[][] Textures, int Length) {
+        TextureRegion[] animationFrames = new TextureRegion[Length];
+        int index = 0;
+        for (int i = 0; i < Length; i++) {
+            animationFrames[index++] = Textures[0][i];
+        }
+        return animationFrames;
+    }
+
+    private static TextureRegion[][] createTextureRegion(Texture SpriteSheet) {
+        int length = SpriteSheet.getWidth() / countFrames(SpriteSheet);
+        return TextureRegion.split(SpriteSheet, length, SpriteSheet.getHeight());
+    }
+
+    private static int countFrames(Texture SpriteSheet) {
+        return SpriteSheet.getWidth() / SpriteSheet.getHeight();
+    }
+
+    private static TextureRegion[][] flipTextures(TextureRegion[][] Textures, int Length) {
+        for (int i = 0; i < Length; i++) {
+            Textures[0][i].flip(true, false);
+        }
+        return Textures;
+    }
+
+    private static float Speed(int Length) {
+        return (Length) * (.1f / fps);
     }
 
 }
