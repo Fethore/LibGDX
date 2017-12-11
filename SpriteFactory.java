c849eaf9eb186c9318fc098a1da6070516b73242
Angel Fernandez <mike.ferndz@outlook.com>
2017-12-11 11:12:50 -0500

    Update and rename SpriteList.java to SpriteFactory.java

---
+++ SpriteFactory.java
@@ -0,0 +1,15 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import java.util.ArrayList;
+
+/** Created by Angel Fernandez on 7/14/2017 */
+public class SpriteFactory {
+  
+    public void create(String SpriteLocation){
+        Texture texture = new Texture(Gdx.files.internal(SpriteLocation));
+        Sprite sprite = new Sprite(texture);
+        return sprite;
+    }
+
+}
