a8915e70784725e22e82c11b3da1a37d592a78c9
Angel Fernandez <mike.ferndz@outlook.com>
2018-02-12 12:58:59 -0500

    Update and rename SpriteFactory.java to SpriteCreator.java

--- SpriteCreator.java
+++ SpriteCreator.java
@@ -3,9 +3,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 /** Created by Angel Fernandez on 7/14/2017 */
-public class SpriteFactory {
+public class SpriteCreator {
   
-    public void create(String SpriteLocation){
+    public static Sprite create(String SpriteLocation){
         Texture texture = new Texture(Gdx.files.internal(SpriteLocation));
         Sprite sprite = new Sprite(texture);
         return sprite;
